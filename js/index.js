053169fa85d5149688ff590e030fde992ee4dc19
Roger Caballero <rogercp1234@yahoo.com>
2019-04-08 13:52:13 -0700

    footer done

--- js/index.js
+++ js/index.js
@@ -38,6 +38,9 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+//navigation
+//////////////////
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
@@ -59,6 +62,9 @@ nav5.textContent="About";
 let nav6=document.querySelector("nav a:nth-child(6)");
 nav6.textContent="Contact";
 
+
+//top section
+/////////////////////
 let centerTxt=document.querySelector(".cta-text h1");
 centerTxt.innerHTML="DOM<br> Is<br> Awesome";
 
@@ -67,6 +73,43 @@ button1.textContent="Get Started";
 
 let roundImage=document.querySelector("#cta-img");
 roundImage.src="img/header-img.png";
+
+
+//middle section
+//////////////////////
+let topContent1=document.querySelector(".top-content div:nth-child(1) h4 "); 
+ topContent1.textContent="Features";
+ let topContent11=document.querySelector(".top-content div:nth-child(1) p");
+ topContent11.textContent="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let topContent2=document.querySelector(".top-content div:nth-child(2) h4");
+topContent2.textContent="About";
+let topContent22=document.querySelector(".top-content div:nth-child(2) p");
+topContent22.textContent="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
+
+
+let bigImage=document.querySelector("#middle-img");
+bigImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+
+
+//footer
+//////////////////
+let footerContentHeader=document.querySelector(".contact h4");
+footerContentHeader.textContent="Contact";
+
+let footerContentP1=document.querySelector(".contact p:nth-child(2)");
+footerContentP1.innerHTML="123 Way 456 Street<br/> Somewhere, USA";
+
+let footerContentP2=document.querySelector(".contact p:nth-child(3)");
+footerContentP2.textContent="1 (888) 888-8888";
+
+let footerContentP3=document.querySelector(".contact p:nth-child(4)");
+footerContentP3.textContent="sales@greatidea.io"
+
+
 
 
 
